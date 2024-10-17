c63d676c3020733d50f6158d88fbca06401afe09
Marco-Guarnera <Darkaquon93@gmail.com>
2024-10-17 16:50:00 +0200

    Completed database_query_group.sql

--- database_query_group.sql
+++ database_query_group.sql
@@ -1,10 +1,19 @@
 -- 1. Contare quanti iscritti ci sono stati ogni anno
-
+SELECT YEAR(`enrolment_date`) AS `year`, COUNT(`id`) AS `students`
+FROM `students`
+GROUP BY `year`;
 
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
-
+SELECT `office_address`, COUNT(`id`) AS `teachers`
+FROM `teachers`
+GROUP BY `office_address`;
 
 -- 3. Calcolare la media dei voti di ogni appello d'esame
-
+SELECT `exam_id`, AVG(`vote`) AS `vote_average`
+FROM `exam_student`
+GROUP BY `exam_id`;
 
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
+SELECT `department_id`, COUNT(`id`) AS `degrees`
+FROM `degrees`
+GROUP BY `department_id`;
